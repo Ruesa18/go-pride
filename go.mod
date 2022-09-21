0a3371cd1daff75986dbaac3b2b572d729c1d160
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-09-21 18:05:11 +0200

    feat: add pride flag

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/gookit/color v1.5.2 // indirect
+	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/gookit/color"
+import (
+	"fmt"
+
+	"github.com/gookit/color"
+)
+
+func displayFlag(flagWidth int, colors []uint8) {
+	for i := 0; i < len(colors); i++ {
+		for x := 0; x < flagWidth; x++ {
+			color.S256(colors[i], colors[i]).Print(" ")
+		}
+		fmt.Println()
+	}
+}
 
 func main() {
-	color.S256(32, 203).Println("Hello World!")
+	flagWidth := 20
+	colors := []uint8{160, 166, 220, 28, 4, 90}
+
+	displayFlag(flagWidth, colors)
 }
